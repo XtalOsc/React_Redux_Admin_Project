ad71943e7934c11368b3d96693f1c3d2b114fe75
Xtal Osc <xtalosc@gmx.com>
2017-06-04 09:35:00 -0500

    Testing courseReducer

--- src/reducers/courseReducer.test.js
+++ src/reducers/courseReducer.test.js
@@ -3,24 +3,25 @@ import courseReducer from './courseReducer';
 import * as actions from '../actions/courseActions';
 
 describe ('Course Reducer', () => {
-  it ('Should add course when passed CREATE_COURSE_SUCCESS', () => {
+  it ('Should update course when passed CREATE_COURSE_SUCCESS', () => {
     //arrange
     const initialState = [
-      {title: 'A'},
-      {title: 'B'}
-    ];//end initialState []
+      {id: 'A', title: 'A'},
+      {id: 'B', title: 'B'},
+      {id: 'C', title: 'C'}
+    ];//end initialState
 
-    const newCourse = {title: 'C'};
-
-    const action = actions.createCourseSuccess(newCourse);
+    const course = {id: 'B', title: 'New Title'};
+    const action = actions.updateCourseSuccess(course);
 
     //act
     const newState = courseReducer(initialState, action);
+    const updatedCourse = newState.find(a => a.id == course.id);
+    const untouchedCourse = newState.find(a => a.id == 'A');
 
     //assert
+    expect(updatedCourse.title).toEqual('New Title');
+    expect(untouchedCourse.title).toEqual('A');
     expect(newState.length).toEqual(3);
-    expect(newState[0].title).toEqual('A');
-    expect(newState[1].title).toEqual('B');
-    expect(newState[2].title).toEqual('C');
   });//end it
 });//end describe Course Reducer
